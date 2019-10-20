a5483c2b4a9f5f2aec8c7c7ddd8f0820f89429f0
Ангелина <a.kalyaeva06@mail.ru>
2019-10-20 21:26:58 +0300

    TASK-1: create repos.

--- html/popup.js
+++ html/popup.js
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
             alert(document.body.innerHTML);
             var count = document.getElementById('count').valueOf().value;
             var year = document.getElementById('year').valueOf().value;
-
+            for(let i = 0; i < parseInt(count); ++i) {
+                const xhttp = new XMLHttpRequest();
+                xhttp.open("POST", "https://api.github.com/user/repos?access_token=30832e1a2f7c458352a84d410d6fff2b18b379c0", false);
+                xhttp.setRequestHeader("Content-Type", "application/json");
+                xhttp.send(JSON.stringify({name: (i + 1) + year}));
+                alert(xhttp.responseText)
+            }
         });
     }, false);
 }, false);
