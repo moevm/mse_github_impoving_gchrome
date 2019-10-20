ea4d3f04803421355a3cb3a3281f88daa855214d
Ангелина <a.kalyaeva06@mail.ru>
2019-10-20 21:56:32 +0300

    TASK-1: remove token.

--- html/popup.js
+++ html/popup.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
             var year = document.getElementById('year').valueOf().value;
             for(let i = 0; i < parseInt(count); ++i) {
                 const xhttp = new XMLHttpRequest();
-                xhttp.open("POST", "https://api.github.com/user/repos?access_token=30832e1a2f7c458352a84d410d6fff2b18b379c0", false);
+                xhttp.open("POST", "https://api.github.com/user/repos?access_token=<TOKEN>", false);
                 xhttp.setRequestHeader("Content-Type", "application/json");
                 xhttp.send(JSON.stringify({name: (i + 1) + year}));
                 alert(xhttp.responseText)
