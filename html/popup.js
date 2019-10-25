d00a104650d5aa86934e5ef97e328a1e6630785e
Zhuravlev Roman <zhuravlevra@gmail.com>
2019-10-25 16:30:41 +0700

    Added token and users inputs

--- html/popup.js
+++ html/popup.js
@@ -1,30 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('createRepos');
+    const checkPageButton = document.getElementById('createRepos');
     checkPageButton.addEventListener('click', function() {
         chrome.tabs.getSelected(null, function(tab) {
             alert(document.body.innerHTML);
-            var count = document.getElementById('count').valueOf().value;
-            var year = document.getElementById('year').valueOf().value;
+            const count = document.getElementById('count').valueOf().value;
+            const year = document.getElementById('year').valueOf().value;
+            const token = document.getElementById('token').valueOf().value;
+            const users = document.getElementById('users').valueOf().value.split(',').map(user => user.trim());
+            const teams = document.getElementById('teams').valueOf().value.split(',').map(team => team.trim());
             for(let i = 0; i < parseInt(count); ++i) {
                 const xhttp = new XMLHttpRequest();
-                xhttp.open("POST", "https://api.github.com/user/repos?access_token=<ACCESS_TOKEN>", false);
+                xhttp.open("POST", "https://api.github.com/user/repos?access_token=" + token, false);
                 xhttp.setRequestHeader("Content-Type", "application/json");
                 xhttp.send(JSON.stringify({name: (i + 1) + year, auto_init: true}));
                 alert(xhttp.responseText);
-                var name = (i + 1) + year;
-                var repo = "<ORGANIZATION_OWNER>" + "/" + (i + 1) + name;
-                xhttp.open("PUT", "https://api.github.com/repos/"+repo+"/branches/master/protection?access_token=<ACCESS_TOKEN>", false);
+                const name = (i + 1) + year;
+                const repo = "<ORGANIZATION_OWNER>" + "/" + (i + 1) + name;//TODO create input for organization owner
+                xhttp.open("PUT", "https://api.github.com/repos/"+repo+"/branches/master/protection?access_token=" + token, false);
                 xhttp.send(JSON.stringify({
                     required_status_checks: null,
                     enforce_admins: null,
                     required_pull_request_reviews: null,
                     restrictions: {
-                        users: [
-                            "<USER_NAME>"
-                        ],
-                        teams: [
-                            "<TEAM_NAME>"
-                        ]
+                        users: users,
+                        teams: teams
                     }
                 }));
                 alert(xhttp.responseText);
