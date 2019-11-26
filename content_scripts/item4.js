6ebd9950983137137d19b3967e0b2f4d6214c047
esiole <31545768+esiole@users.noreply.github.com>
2019-11-26 16:44:13 +0300

    4 таска, добавление использования авторизации в 5 такску, небольшой рефакторинг

--- content_scripts/item4.js
+++ content_scripts/item4.js
@@ -1,19 +1,127 @@
-let countComment = 3;
-
 setInterval(() => {
-    let commentBlock = document.getElementsByClassName("previewable-edit reorderable-task-lists project-comment-body-hover js-comment js-task-list-container");
-    if(commentBlock[0] !== undefined) {
-        //console.log(commentBlock[0].childElementCount);
-        if(commentBlock[0].childElementCount == 4) {
-            let commentDiv = document.createElement("div");
-            commentDiv.id = "comments";
-            for(let i = 0; i < countComment; i++) {
-                let comment = document.createElement("div");
-                comment.innerHTML = "Комментарий!";
-                //commentBlock[0].appendChild(comment);
-                commentDiv.appendChild(comment);
-            }
-            commentBlock[0].appendChild(commentDiv);
-        }
-    }
+    chrome.storage.local.get((['token'], (res) => {
+        let token = res['token'];
+        let url = window.location.pathname.split("/");
+        let path = `https://api.github.com/repos/${url[1]}/${url[2]}/issues/`;
+        createComments(path, token);
+    }));
 }, 100);
+
+function createComments(partPath, token) {
+    let commentBlock = document.getElementsByClassName("previewable-edit reorderable-task-lists project-comment-body-hover js-comment js-task-list-container");
+    if (commentBlock[0] !== undefined && commentBlock[0].childElementCount === 4) {
+        let commentDiv = document.createElement("div");
+        commentBlock[0].appendChild(commentDiv);
+        let numberSpan = document.getElementsByClassName('link-gray-dark js-project-card-details-external-link');
+        let numberIssue = numberSpan[0].childNodes[3].innerHTML.replace('#', '');
+        let path = `${partPath}${numberIssue}/comments?access_token=${token}`;
+        let comments = [];
+
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function (data) {
+            if (this.readyState === 4 && this.status === 200) {
+                let responseData = JSON.parse(data.target.response);
+                for (let elem of responseData) {
+                    let comment = {
+                        'comment': elem.body,
+                        'date': elem.created_at,
+                        'user': elem.user.login,
+                        'avatar': elem.user.avatar_url,
+                        'id': elem.user.id
+                    };
+                    comments.push(comment);
+                }
+                commentDiv.className = ' comments';
+                for (let elem of comments) createElement(elem, commentDiv);
+                let iconCountComments = document.getElementsByClassName('octicon octicon-comment v-align-middle');
+                let button = iconCountComments[0].parentNode;
+                button.style.cursor = 'pointer';
+                button.addEventListener('click', (event) => {
+                    let commentsBlock = document.getElementsByClassName('comments')[0];
+                    if (commentsBlock.style.display === 'none') {
+                        commentsBlock.style.display = '';
+                    } else {
+                        commentsBlock.style.display = 'none';
+                    }
+                });
+            }
+        };
+        xhttp.open("GET", path, true);
+        xhttp.send();
+    }
+}
+
+function createElement(elem, commentDiv) {
+    let container = document.createElement("div");
+    container.className += ' pt-3 px-3 d-flex';
+    let avatarContainer = document.createElement("div");
+    avatarContainer.className += ' pr-2';
+    let avatar = document.createElement("a");
+    avatar.className += ' js-hovercard-left';
+    avatar.setAttribute('data-hovercard-type', 'user');
+    avatar.setAttribute('data-hovercard-url', `/users/${elem.user}/hovercard`);
+    avatar.setAttribute('data-octo-click', 'hovercard-link-click');
+    avatar.setAttribute('data-octo-dimensions', 'link_type:self');
+    avatar.setAttribute('href', `/${elem.user}`);
+    let icon = document.createElement('img');
+    icon.className += 'avatar';
+    icon.setAttribute('height', `44`);
+    icon.setAttribute('width', `44`);
+    icon.setAttribute('alt', `@${elem.user}`);
+    icon.src = elem.avatar;
+    avatar.appendChild(icon);
+    avatarContainer.appendChild(avatar);
+
+    let head = document.createElement('div');
+    head.className += ' flex-auto overflow-hidden';
+    let name = document.createElement('a');
+    name.className += ' text-bold text-gray js-hovercard-left';
+    name.setAttribute('data-hovercard-type', 'user');
+    name.setAttribute('data-hovercard-url', `/users/${elem.user}/hovercard`);
+    name.setAttribute('data-octo-click', 'hovercard-link-click');
+    name.setAttribute('data-octo-dimensions', 'link_type:self');
+    name.setAttribute('href', `/${elem.user}`);
+    name.innerHTML = `${elem.user}`;
+    let dateContainer = document.createElement('div');
+    dateContainer.className += ' text-gray';
+    dateContainer.innerHTML = 'commented ';
+    let relativeTime = document.createElement('relative-time');
+    relativeTime.className += ' no-wrap';
+    relativeTime.setAttribute('datetime', elem.date);
+    dateContainer.appendChild(relativeTime);
+
+    head.appendChild(name);
+    head.appendChild(dateContainer);
+
+    let panel = document.createElement('div');
+    panel.className += ' edit-comment-hide';
+    let detailes = document.createElement('div');
+    detailes.className += ' Details js-details-container';
+    panel.appendChild(detailes);
+    let pos = document.createElement('div');
+    pos.className += ' project-issue-body-wrapper position-relative js-project-issue-body-wrapper';
+    detailes.appendChild(pos);
+    let last = document.createElement('div');
+    last.className += ' js-project-issue-body-container';
+    pos.appendChild(last);
+    let table = document.createElement('table');
+    table.className += ' d-block';
+    last.appendChild(table);
+    let tr = document.createElement('tr');
+    tr.className += ' d-block';
+    table.appendChild(tr);
+    let td = document.createElement('td');
+    td.className += 'd-block comment-body markdown-body  js-comment-body';
+    tr.appendChild(td);
+    let p = document.createElement('p');
+    p.className += ' text-gray';
+    td.appendChild(p);
+    let text = document.createElement('em');
+    text.innerHTML = `${elem.comment}`;
+    p.appendChild(text);
+
+    container.appendChild(avatarContainer);
+    container.appendChild(head);
+    commentDiv.appendChild(container);
+    commentDiv.appendChild(panel);
+}
--- content_scripts/item5.js
+++ content_scripts/item5.js
@@ -1,20 +1,30 @@
-setTimeout(function() {
+chrome.extension.onMessage.addListener(function(request, sender, response) {
+    if (request.type === 'updatePage') {
+        chrome.storage.local.get((['token'], (res) => {
+            let token = res['token'];
+            let url = window.location.pathname.split("/");
+            let path = `https://api.github.com/repos/${url[1]}/${url[2]}/commits?access_token=${token}`;
+            createCommitsList(path, url);
+        }));
+    }
+    return true;
+});
+
+function createCommitsList(path, url) {
     let label = document.getElementsByClassName("commit-tease js-details-container Details d-flex rounded-top-1");
-    if(label[0] !== undefined) {
-        let partUrl = window.location.pathname.split("/");
-        let path = `https://api.github.com/repos/${partUrl[1]}/${partUrl[2]}/commits`;
+    if (label[0] !== undefined && label.length === 1) {
         let usersCommits = [];
         function findName(name) {
-            for(let elem of usersCommits) if(elem.name == name) return true;
+            for (let elem of usersCommits) if(elem.name === name) return true;
             return false;
         }
 
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function (data) {
-            if(this.readyState == 4 && this.status == 200) {
+            if (this.readyState === 4 && this.status === 200) {
                 let responseData = JSON.parse(data.target.response);
-                for(let elem of responseData) {
-                    if(findName(elem.commit.author.name)) continue;
+                for (let elem of responseData) {
+                    if (findName(elem.commit.author.name)) continue;
                     let lastCommitUser = {
                         'name': elem.commit.author.name,
                         'commit': elem.commit.message,
@@ -36,76 +46,12 @@ setTimeout(function() {
                 select.appendChild(textSelect);
                 label[0].before(select);
 
-                for(let elem of usersCommits) {
-                    let container = document.createElement("div");
-                    container.className += ' commit-tease js-details-container Details rounded-top-1 optionCommit';
-                    container.style.display = 'none';
-                    let avatarContainer = document.createElement("div");
-                    avatarContainer.className += ' AvatarStack flex-self-start';
-                    let avatar = document.createElement("div");
-                    avatar.className += ' AvatarStack-body';
-                    avatar.setAttribute('aria-label', `${elem.name}`);
-                    let refImg = document.createElement('a');
-                    refImg.className += ' avatar';
-                    refImg.setAttribute('data-skip-pjax', 'true');
-                    refImg.setAttribute('data-hovercard-type', 'user');
-                    refImg.setAttribute('data-hovercard-url', `/hovercards?user_id=${elem.id}`);
-                    refImg.setAttribute('data-octo-click', 'hovercard-link-click');
-                    refImg.setAttribute('data-octo-dimensions', 'link_type:self');
-                    refImg.setAttribute('href', `/${elem.name}`);
-                    let icon = document.createElement('img');
-                    icon.setAttribute('height', `20`);
-                    icon.setAttribute('width', `20`);
-                    icon.setAttribute('alt', `@${elem.name}`);
-                    icon.src = elem.avatar;
-                    avatarContainer.appendChild(avatar);
-                    avatar.appendChild(refImg);
-                    refImg.appendChild(icon);
-
-                    let info = document.createElement('div');
-                    info.className += ' flex-auto f6 mr-3';
-                    let userRef = document.createElement('a');
-                    userRef.className += ' commit-author tooltipped tooltipped-s user-mention';
-                    userRef.setAttribute('href', `/${partUrl[1]}/${partUrl[2]}/commits?author=${elem.name}`);
-                    userRef.setAttribute('aria-label', `View all commits by ${elem.name}`);
-                    let commitRef = document.createElement('a');
-                    commitRef.className += ' message text-inherit';
-                    commitRef.setAttribute('data-pjax', 'true');
-                    commitRef.setAttribute('title', elem.commit);
-                    commitRef.setAttribute('href', `/${partUrl[1]}/${partUrl[2]}/commit/${elem.sha}`);
-                    userRef.innerHTML = `${elem.name} `;
-                    commitRef.innerHTML = elem.commit;
-                    info.appendChild(userRef);
-                    info.appendChild(commitRef);
-
-                    let commitAndDate = document.createElement('div');
-                    commitAndDate.className += ' no-wrap d-flex flex-self-start flex-items-baseline';
-                    let pred = document.createElement('span');
-                    pred.className += ' mr-1';
-                    pred.innerHTML = 'Latest commit';
-                    let shaRef = document.createElement('a');
-                    shaRef.className += ' commit-tease-sha mr-1';
-                    shaRef.innerHTML = elem.sha.substring(0, 7);
-                    shaRef.setAttribute('href', `/${partUrl[1]}/${partUrl[2]}/commit/${elem.sha}`);
-                    let date = document.createElement('span');
-                    let relativeTime = document.createElement('relative-time');
-                    relativeTime.className += ' no-wrap';
-                    relativeTime.setAttribute('datetime', elem.date);
-                    date.appendChild(relativeTime);
-                    commitAndDate.appendChild(pred);
-                    commitAndDate.appendChild(shaRef);
-                    commitAndDate.appendChild(date);
-
-                    container.appendChild(avatarContainer);
-                    container.appendChild(info);
-                    container.appendChild(commitAndDate);
-                    select.after(container);
-                }
+                for (let elem of usersCommits) createElement(elem, url, select);
 
                 let toggle = false;
                 document.getElementById("selectCommits").onclick = () => {
                     let commits = document.getElementsByClassName("optionCommit");
-                    if(toggle) {
+                    if (toggle) {
                         textSelect.innerHTML = 'SHOW last users commit';
                         for(let elem of commits) {
                             elem.classList.remove('d-flex');
@@ -125,4 +71,70 @@ setTimeout(function() {
         xhttp.open("GET", path, true);
         xhttp.send();
     }
-}, 500);
+}
+
+function createElement(elem, url, select) {
+    let container = document.createElement("div");
+    container.className += ' commit-tease js-details-container Details rounded-top-1 optionCommit';
+    container.style.display = 'none';
+    let avatarContainer = document.createElement("div");
+    avatarContainer.className += ' AvatarStack flex-self-start';
+    let avatar = document.createElement("div");
+    avatar.className += ' AvatarStack-body';
+    avatar.setAttribute('aria-label', `${elem.name}`);
+    let refImg = document.createElement('a');
+    refImg.className += ' avatar';
+    refImg.setAttribute('data-skip-pjax', 'true');
+    refImg.setAttribute('data-hovercard-type', 'user');
+    refImg.setAttribute('data-hovercard-url', `/hovercards?user_id=${elem.id}`);
+    refImg.setAttribute('data-octo-click', 'hovercard-link-click');
+    refImg.setAttribute('data-octo-dimensions', 'link_type:self');
+    refImg.setAttribute('href', `/${elem.name}`);
+    let icon = document.createElement('img');
+    icon.setAttribute('height', `20`);
+    icon.setAttribute('width', `20`);
+    icon.setAttribute('alt', `@${elem.name}`);
+    icon.src = elem.avatar;
+    avatarContainer.appendChild(avatar);
+    avatar.appendChild(refImg);
+    refImg.appendChild(icon);
+
+    let info = document.createElement('div');
+    info.className += ' flex-auto f6 mr-3';
+    let userRef = document.createElement('a');
+    userRef.className += ' commit-author tooltipped tooltipped-s user-mention';
+    userRef.setAttribute('href', `/${url[1]}/${url[2]}/commits?author=${elem.name}`);
+    userRef.setAttribute('aria-label', `View all commits by ${elem.name}`);
+    let commitRef = document.createElement('a');
+    commitRef.className += ' message text-inherit';
+    commitRef.setAttribute('data-pjax', 'true');
+    commitRef.setAttribute('title', elem.commit);
+    commitRef.setAttribute('href', `/${url[1]}/${url[2]}/commit/${elem.sha}`);
+    userRef.innerHTML = `${elem.name} `;
+    commitRef.innerHTML = elem.commit;
+    info.appendChild(userRef);
+    info.appendChild(commitRef);
+
+    let commitAndDate = document.createElement('div');
+    commitAndDate.className += ' no-wrap d-flex flex-self-start flex-items-baseline';
+    let pred = document.createElement('span');
+    pred.className += ' mr-1';
+    pred.innerHTML = 'Latest commit';
+    let shaRef = document.createElement('a');
+    shaRef.className += ' commit-tease-sha mr-1';
+    shaRef.innerHTML = elem.sha.substring(0, 7);
+    shaRef.setAttribute('href', `/${url[1]}/${url[2]}/commit/${elem.sha}`);
+    let date = document.createElement('span');
+    let relativeTime = document.createElement('relative-time');
+    relativeTime.className += ' no-wrap';
+    relativeTime.setAttribute('datetime', elem.date);
+    date.appendChild(relativeTime);
+    commitAndDate.appendChild(pred);
+    commitAndDate.appendChild(shaRef);
+    commitAndDate.appendChild(date);
+
+    container.appendChild(avatarContainer);
+    container.appendChild(info);
+    container.appendChild(commitAndDate);
+    select.after(container);
+}
