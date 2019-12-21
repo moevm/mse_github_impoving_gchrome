7e99a4133e2dfc809b493abcb7b9df6b03d80ca5
Ангелина <a.kalyaeva06@mail.ru>
2019-12-21 14:35:01 +0300

    Task 3 done.

--- content_scripts/item3.js
+++ content_scripts/item3.js
@@ -1,6 +1,6 @@
 setInterval(() => {
   
-let short_task_info = document.getElementsByClassName("link-gray-dark js-project-card-details-external-link");
+let short_task_info = document.getElementsByClassName("Details-content--shown");
 for (let i = 0; i < short_task_info.length; ++i) {
     if (short_task_info[i].childElementCount < 3) {
         if (short_task_info[i] !== undefined) {
@@ -12,6 +12,25 @@ for (let i = 0; i < short_task_info.length; ++i) {
 
             let lineTask3 = document.createElement("button");
             lineTask3.id = "copy_task_button";
+            lineTask3.onclick= function () {
+                let titleList = document.getElementsByClassName("text-bold js-issue-title");
+                let title = titleList[0];
+                let bodyList= document.getElementsByClassName("d-block comment-body markdown-body  js-comment-body");
+                let body = bodyList[0];
+                chrome.storage.local.get((['token'], (res) => {
+                    let token = res['token'];
+                    let url = window.location.pathname.split("/");
+                    let path = `https://api.github.com/repos/${url[1]}/${url[2]}/issues`;
+                    const xhttp = new XMLHttpRequest();
+                    xhttp.open("POST", path + "?access_token=" + token, false);
+                    xhttp.setRequestHeader("Content-Type", "application/json");
+                    xhttp.send(JSON.stringify({
+                        title: title.innerHTML,
+                        body: body.innerText,
+                    }));
+                    document.location.reload(true);
+                }));
+            };
             lineTask3.classList.add("btn-link");
             lineTask3.classList.add("muted-link");
             lineTask3.innerHTML = "Clone task";
